687b5b496f7302bb1a651ca770ec61dace0e9be8
Sergey Alimpiev <justwmz509@gmail.com>
2021-06-23 14:44:06 +0300

    Add base templates, add first model for the posts.

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.urls import path
+from blog.views import *
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+]
